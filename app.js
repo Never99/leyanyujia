03b709fb77a2bb70ec4d4fdb2c9818c7695ea67c
yyd <t-yangyadong@163.com>
2017-12-06 21:01:49 +0800

    新修改了bug

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    // 我是新加进来没
     // 登录
     wx.login({
       success: res => {
